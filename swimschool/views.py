045823c040baa02f869d0a259c84e545712b2896
Ygallo <yaninagallo@gmail.com>
2023-09-09 18:00:43 +0000

    Added hero image to home page

--- swimschool/views.py
+++ swimschool/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.views import generic, View
 
 # Create your views here.
 
 
-def index(request):
-    """Index page view """
-    return render(request, 'index.html')
+# def index(request):
+#     """Index page view """
+#     return render(request, 'index.html')
+
+
+class Home(generic.TemplateView):
+    """
+    View to display the home page
+    """
+    template_name = "index.html"
