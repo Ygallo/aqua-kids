8e1eb83a2299f122c029c48d13accd756dcc89e0
Ygallo <yaninagallo@gmail.com>
2023-09-11 17:36:15 +0000

    Added Course model

--- courses/models.py
+++ courses/models.py
@@ -19,3 +19,52 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Course(models.Model):
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    SEAHORSE_BEGGINERS = 'SH'
+    JELLYFISH_FLOATERS = 'JF'
+    TURTLE_GLIDERS = 'TG'
+    DOLPHIN_DIVERS = 'DD'
+    SHARK_SPEEDSTERS = 'SS'
+    LEVEL = [
+        (SEAHORSE_BEGGINERS, 'seahorse_begginers'),
+        (JELLYFISH_FLOATERS, 'jellyfish_floaters'),
+        (TURTLE_GLIDERS, 'turtle_gliders'),
+        (DOLPHIN_DIVERS, 'dolphin_divers'),
+        (SHARK_SPEEDSTERS, 'shark_speedsters'),
+    ]
+    level = models.CharField(
+        max_length=2,
+        choices=LEVEL,
+        default=SEAHORSE_BEGGINERS,
+    )
+    start_date = models.DateTimeField(auto_now=False)
+    end_date = models.DateTimeField(auto_now=False)
+    MONDAY = 'MO'
+    TUESDAY = 'TU'
+    WEDNESDAY = 'WE'
+    THRUSDAY = 'TH'
+    FRIDAY = 'FR'
+    SATURDAY = 'SA'
+    SUNDAY = 'SU'
+    DAY_OF_THE_WEEK = [
+        (MONDAY, 'monday'),
+        (TUESDAY, 'tuesday'),
+        (WEDNESDAY, 'wednesday'),
+        (THRUSDAY, 'thrusday'),
+        (FRIDAY, 'friday'),
+        (SATURDAY, 'saturday'),
+        (SUNDAY, 'sunday'),
+    ]
+    day_of_the_week = models.CharField(
+        max_length=2,
+        choices=DAY_OF_THE_WEEK,
+    )
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+    places = models.PositiveIntegerField(default=6)
+
+    def __str__(self):
+        return self.name
