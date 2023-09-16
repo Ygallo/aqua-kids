eced162848eab37f68abae8588c871f23e98c2f1
Ygallo <yaninagallo@gmail.com>
2023-09-16 11:42:31 +0000

    Updated cart template

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,16 +1,32 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from courses.models import Course
 
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
-    product_count = 0
+    course_count = 0
+    cart = request.session.get('cart', {})
+
+    for course_id, quantity in cart.items():
+        course = get_object_or_404(Course, pk=course_id)
+        total += quantity * course.price
+        course_count += quantity
+        cart_items.append({
+           'item_id': course_id,
+           'quantity': quantity,
+           'course': course,
+        })
+
+    grand_total = total
 
     context = {
         'cart_items': cart_items,
         'total': total,
-        'product_count': product_count,
+        'course_count': course_count,
+        'grand_total': grand_total,
     }
 
     return context
