156ba45be0db3e84edc81b5b961aa430c7235484
Ygallo <yaninagallo@gmail.com>
2023-09-17 11:16:23 +0000

    Added calc_subtotal template filter

--- cart/views.py
+++ cart/views.py
@@ -26,20 +26,19 @@ def add_to_cart(request, item_id):
     return redirect(redirect_url)
 
 
-# def adjust_cart(request, course_id):
-#     """Adjust the quantity of the specified product to the specified amount"""
+def adjust_cart(request, course_id):
+    """Adjust the quantity of the specified course to the specified amount"""
 
-#     quantity = int(request.POST.get('quantity'))
-#     cart = request.session.get('cart', {})
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
 
-#     if quantity > 0:
-#         cart[course_id] = quantity
-        
-#     else:
-#         cart.pop(course_id)
+    if quantity > 0:
+        cart[course_id] = quantity
+    else:
+        cart.pop(course_id)
 
-#     request.session['cart'] = cart
-#     return redirect(reverse('view_cart'))
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
 
 
 def remove_from_cart(request, course_id):
@@ -56,5 +55,5 @@ def remove_from_cart(request, course_id):
         return HttpResponse(status=200)
 
     except Exception as e:
-        messages.error(request, f'Error removing item: {e}' )
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
