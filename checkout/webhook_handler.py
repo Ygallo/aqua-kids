c5b72b95db22c3d305d8bac80c61bd7c5f60e162
Ygallo <yaninagallo@gmail.com>
2023-09-19 09:49:14 +0000

    Added cache_checkout_data and payment intent logic

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -6,13 +6,13 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
-    
+
     def handle_event(self, event):
         """Handle generi/unexpected webhook event"""
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
-    
+
     def handle_payment_intent_succeeded(self, event):
         """
         Handle the payment_intent.succeeded webhook from Stripe
@@ -27,7 +27,65 @@ class StripeWH_Handler:
             intent.latest_charge
         )
 
+        billing_details = stripe_charge.billing_details # updated
         grand_total = round(stripe_charge.amount / 100, 2) # updated
+
+        # Clean data in the shipping details
+        for field, value in shipping_details.address.items():
+            if value == "":
+                shipping_details.address[field] = None
+
+        order_exists = False
+        try:
+            order = Order.objects.get(
+                full_name__iexact=shipping_details.name,
+                email__iexact=billing_details.email,
+                phone_number__iexact=shipping_details.phone,
+                eircode__iexact=shipping_details.eircode,
+                grand_total=grand_total,
+                original_cart=cart,
+                stripe_pid=pid,
+            )        
+            order_exists = True
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
+                status=200)
+        except Order.DoesNotExist:
+            try:
+                order = Order.objects.create(
+                    full_name__iexact=shipping_details.name,
+                    email__iexact=billing_details.email,
+                    phone_number__iexact=shipping_details.phone,
+                    eircode__iexact=shipping_details.eircode, 
+                )
+                for item_id, item_data in json.loads(cart).items():
+                    course = Course.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            course=course,
+                            quantity=item_data,
+                        )                        
+                        order_line_item.save()
+                    else:
+                        for quantity in items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                course=course,
+                                quantity=quantity,
+                            )
+                            order_line_item.save()
+            except Exception as e:
+                if order:
+                    order.delete()
+                return HttpResponse(
+                    content=f'Webhook received: {event["type"]} | ERROR: {e}',
+                    status=500)
+        # self._send_confirmation_email(order)
+
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
+            status=200)
 
     def handle_payment_intent_payment_failed(self, event):
         """
